bfda6a51f732d0feffc1686574645d4b86066d7d
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-10-11 19:38:26 -0300

    Modificando sinais
    Trocando os dois pontos das variaveis (errado) para o igual (certo)

--- Exercicios012/desafio-36-mundo02.py
+++ Exercicios012/desafio-36-mundo02.py
@@ -9,7 +9,7 @@ valor_da_casa: float(input("Qual o valor da casa? "))
 quantos_anos_aluguel: float(input("Quantos anos pretende pagar o aluguel? "))
 
 #Cálculo do valor da prestação mensal: 
-cálculo_prestação_mensal: (valor_da_casa/quantos_anos_aluguel)
+cálculo_prestação_mensal: valor_da_casa / (quantos_anos_aluguel*12)
 
 #Cálculo caso exceder 30% o valor do salario: 
 exceder_do_salario: (salario_usuário*0.30) 
@@ -18,3 +18,8 @@ exceder_do_salario: (salario_usuário*0.30)
 if salario_usuário > exceder_do_salario: 
     print("O aluguel que você irá pagar é de {}.".format(cálculo_prestação_mensal))
     print("Porém o valor excedeu 30%, sendo assim, seu emprestimo foi negado!")
+else: 
+    print("O aluguel que você irá pagar é de {}.".format(cálculo_prestação_mensal))
+    print("O valor NÃO excedeu 30%, emprestimo APROVADO!")
+
+print("Obrigada por usar nosso programa! Volte sempre!")
