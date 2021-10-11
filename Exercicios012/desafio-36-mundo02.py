47a363216a8fda874b1aa7082aec91046d9ddd4a
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-10-11 19:25:23 -0300

    Adiçao da funçao print
    Adiçao da funçao print na linha 4.

---
+++ Exercicios012/desafio-36-mundo02.py
@@ -0,0 +1,20 @@
+#DESAFIO 36 - EMPRESTIMO BANCARIO 
+
+#Conduta da "empresa"
+print("Apenas se seu salário não exceder 30% da prestação do aluguel, seu emprestimo dará APROVADO. Caso contrário, NEGADO!")
+
+#Perguntando ao usuário o salario dele, o valor da casa e em quantos anos ele irá pagar:
+salario_usuário: float(input("Qual o valor do seu salário? "))
+valor_da_casa: float(input("Qual o valor da casa? "))
+quantos_anos_aluguel: float(input("Quantos anos pretende pagar o aluguel? "))
+
+#Cálculo do valor da prestação mensal: 
+cálculo_prestação_mensal: (valor_da_casa/quantos_anos_aluguel)
+
+#Cálculo caso exceder 30% o valor do salario: 
+exceder_do_salario: (salario_usuário*0.30) 
+
+#Se, senão se, senão: 
+if salario_usuário > exceder_do_salario: 
+    print("O aluguel que você irá pagar é de {}.".format(cálculo_prestação_mensal))
+    print("Porém o valor excedeu 30%, sendo assim, seu emprestimo foi negado!")
