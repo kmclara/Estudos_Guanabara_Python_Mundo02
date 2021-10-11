0f8e79e423ffbaaf577aa3f0cc2aab53ffbb0c51
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-10-11 19:40:13 -0300

    Adicionando formataçao
    Adicionei :.2f nas linhas 19 e 22.

--- Exercicios012/desafio-36-mundo02.py
+++ Exercicios012/desafio-36-mundo02.py
@@ -4,22 +4,22 @@
 print("Apenas se seu salário não exceder 30% da prestação do aluguel, seu emprestimo dará APROVADO. Caso contrário, NEGADO!")
 
 #Perguntando ao usuário o salario dele, o valor da casa e em quantos anos ele irá pagar:
-salario_usuário: float(input("Qual o valor do seu salário? "))
-valor_da_casa: float(input("Qual o valor da casa? "))
-quantos_anos_aluguel: float(input("Quantos anos pretende pagar o aluguel? "))
+salario_usuário = float(input("Qual o valor do seu salário? "))
+valor_da_casa = float(input("Qual o valor da casa? "))
+quantos_anos_aluguel = float(input("Quantos anos pretende pagar o aluguel? "))
 
 #Cálculo do valor da prestação mensal: 
-cálculo_prestação_mensal: valor_da_casa / (quantos_anos_aluguel*12)
+cálculo_prestação_mensal =  valor_da_casa / (quantos_anos_aluguel*12)
 
 #Cálculo caso exceder 30% o valor do salario: 
-exceder_do_salario: (salario_usuário*0.30) 
+exceder_do_salario = (salario_usuário*0.30) 
 
 #Se, senão se, senão: 
 if salario_usuário > exceder_do_salario: 
-    print("O aluguel que você irá pagar é de {}.".format(cálculo_prestação_mensal))
+    print("O aluguel que você irá pagar é de {:.2f}.".format(cálculo_prestação_mensal))
     print("Porém o valor excedeu 30%, sendo assim, seu emprestimo foi negado!")
 else: 
-    print("O aluguel que você irá pagar é de {}.".format(cálculo_prestação_mensal))
+    print("O aluguel que você irá pagar é de {:.2f}.".format(cálculo_prestação_mensal))
     print("O valor NÃO excedeu 30%, emprestimo APROVADO!")
 
 print("Obrigada por usar nosso programa! Volte sempre!")
