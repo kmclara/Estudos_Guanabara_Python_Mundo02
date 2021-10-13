be154b62f39f9800fb4bd14887fadb5bea519966
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-10-13 14:50:17 -0300

    Remoçao da atribuiçao de valor maior a variavel 1

--- Exercicios012/desafio-38-mundo02.py
+++ Exercicios012/desafio-38-mundo02.py
@@ -3,3 +3,14 @@
 #Pedindo ao usuário dois valores numericos: 
 número_usuário_um = int(input("Digite o primeiro valor: "))
 número_usuário_dois = int(input("Digite o segundo valor: "))
+
+
+#Verificando quem é maior:
+if número_usuário_dois>número_usuário_um: 
+    print("O número {} é maior que o número {}!".format(número_usuário_dois, número_usuário_um))
+elif número_usuário_um>número_usuário_dois: 
+    print("O número {} é maior que o número {}!".format(número_usuário_um, número_usuário_dois))
+elif número_usuário_um == número_usuário_dois: 
+    print("Ambos os números {} e {} são valores iguais!".format(número_usuário_um, número_usuário_dois))
+
+print("Obrigada por usar nosso programa! Volte sempre!")
