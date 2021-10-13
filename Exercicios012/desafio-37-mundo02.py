741a2df71c25967fa33733eeda9c771ff9dcb912
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-10-13 14:24:42 -0300

    Adicionando fatiamento nas condiçoes aninhadas
    Para que nao apareça letras no resultado que vai ser mostrado ao usuario, adicionei o tratamento de string fatiamento.
    > Toda vez que o usuario fosse receber o resultado, por padrao, iria mostrar uma letra junto ao resultado, como: Se o usuario converter tal numero em binario, iria voltar 0b, que quer dizer que foi convertido para binario e logo depois do 0b, iria dar o resultado, e assim se faria com a conversao do hexadecial e octal.

--- Exercicios012/desafio-37-mundo02.py
+++ Exercicios012/desafio-37-mundo02.py
@@ -10,3 +10,13 @@ print("""Escolha uma das bases para que se realize a conversão:
 [3] HEXADECIMAL """)
 
 Opção_do_usuário = int(input("Sua opção: "))
+
+#If, Elif or Else
+if Opção_do_usuário == 1: 
+    print("{} convertido para BINÁRIO é igual á {}!".format(número_inteiro,bin(número_inteiro) [2: ] ))
+elif Opção_do_usuário == 2:
+    print("{} convertido para OCTAL é igual á {}.".format(número_inteiro, oct(número_inteiro) [2: ]))
+elif Opção_do_usuário == 3:
+    print("{} convertido para HEXADECIMAL é igual á {}.".format(número_inteiro, hex(número_inteiro) [2: ]))
+else: 
+    print("Opção invalida! Tente novamente!")
