9d3b15dbad7fee7944b2e61ea99c947f33e252e9
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-10-13 14:40:56 -0300

    Adicionando duas variaveis e trocando aspas simples por aspas duplas.

---
+++ Exercicios012/desafio-38-mundo02.py
@@ -0,0 +1,5 @@
+#DESAFIO 38 - QUAL NÚMERO É MAIOR, MENOR OU IGUAL? 
+
+#Pedindo ao usuário dois valores numericos: 
+número_usuário_um = int(input("Digite o primeiro valor: "))
+número_usuário_dois = int(input("Digite o segundo valor: "))
