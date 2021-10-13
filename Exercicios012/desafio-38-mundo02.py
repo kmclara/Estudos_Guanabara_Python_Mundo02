2c38afa77ad3a06ff3049c5b23eaf644f4c56450
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-10-13 15:09:24 -0300

    Retirado ifs, elifs e prints
    Pois nao eram necessarios

--- Exercicios012/desafio-38-mundo02.py
+++ Exercicios012/desafio-38-mundo02.py
@@ -8,15 +8,9 @@ número_usuário_dois = int(input("Digite o segundo valor: "))
 #Verificando quais são maior, menor e igual: 
 if número_usuário_dois>número_usuário_um: 
     print("O número {} é maior que o número {}!".format(número_usuário_dois, número_usuário_um))
-elif número_usuário_um<número_usuário_dois:
-    print("O número {} é menor que o número {}!".format(número_usuário_um, número_usuário_dois))
-if número_usuário_um>número_usuário_dois: 
+elif número_usuário_um>número_usuário_dois: 
     print("O número {} é maior que o número {}!".format(número_usuário_um, número_usuário_dois))
-elif número_usuário_dois<número_usuário_um: 
-    print("O número {} é menor que o número {}!".format(número_usuário_dois, número_usuário_um))
-if número_usuário_um == número_usuário_dois: 
+elif número_usuário_um == número_usuário_dois: 
     print("Ambos os números {} e {} são valores iguais!".format(número_usuário_um, número_usuário_dois))
-elif número_usuário_dois == número_usuário_um:
-    print("Ambos os números {} e {} são valores iguais!".format(número_usuário_dois, número_usuário_um))
 
 print("Obrigada por usar nosso programa! Volte sempre!")
