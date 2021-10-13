9ba89cf55a497c04e9675f5a0619c4b3538561c8
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-10-13 15:04:22 -0300

    Adicionado if, elif e print em algumas linhas.

--- Exercicios012/desafio-38-mundo02.py
+++ Exercicios012/desafio-38-mundo02.py
@@ -5,16 +5,18 @@ número_usuário_um = int(input("Digite o primeiro valor: "))
 número_usuário_dois = int(input("Digite o segundo valor: "))
 
 
-#Verificando quem é maior:
+#Verificando quais são maior, menor e igual: 
 if número_usuário_dois>número_usuário_um: 
     print("O número {} é maior que o número {}!".format(número_usuário_dois, número_usuário_um))
 elif número_usuário_um<número_usuário_dois:
     print("O número {} é menor que o número {}!".format(número_usuário_um, número_usuário_dois))
-elif número_usuário_um>número_usuário_dois: 
+if número_usuário_um>número_usuário_dois: 
     print("O número {} é maior que o número {}!".format(número_usuário_um, número_usuário_dois))
 elif número_usuário_dois<número_usuário_um: 
     print("O número {} é menor que o número {}!".format(número_usuário_dois, número_usuário_um))
-elif número_usuário_um == número_usuário_dois: 
+if número_usuário_um == número_usuário_dois: 
     print("Ambos os números {} e {} são valores iguais!".format(número_usuário_um, número_usuário_dois))
+elif número_usuário_dois == número_usuário_um:
+    print("Ambos os números {} e {} são valores iguais!".format(número_usuário_dois, número_usuário_um))
 
 print("Obrigada por usar nosso programa! Volte sempre!")
