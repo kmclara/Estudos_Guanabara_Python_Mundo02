2f230ce79b8c3cac918edd8e530fa390731984b0
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-10-13 14:06:15 -0300

    Mudando os sinais
    Mudando o sinal : nas variaveis para =, pois esqueci que na verdade se usa = ao inves de :

---
+++ Exercicios012/desafio-37-mundo02.py
@@ -0,0 +1,12 @@
+#DESAFIO 37 - CONVERSOR DE BASES NÚMERICAS
+
+#Pedindo ao usuário um número inteiro para a conversão: 
+número_inteiro = int(input("Digite um número inteiro: "))
+
+#Pedindo ao usuário que escolha uma das 3 bases para realizar a conversão: 
+print("""Escolha uma das bases para que se realize a conversão: 
+[1] BINÁRIO
+[2] OCTAL 
+[3] HEXADECIMAL """)
+
+Opção_do_usuário = int(input("Sua opção: "))
