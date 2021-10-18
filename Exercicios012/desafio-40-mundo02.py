763346ff7040bf5b5fcc5f712fb5ce600e93506b
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-10-18 16:40:27 -0300

    Mudança de > (maior) para < (menor) na condicional if

--- Exercicios012/desafio-40-mundo02.py
+++ Exercicios012/desafio-40-mundo02.py
@@ -1,1 +1,11 @@
 #DESAFIO 40 - MÉDIA DE NOTAS
+
+#Perguntando ao aluno duas notas para calcular a média dele: 
+Nota_1 = float(input("Adicione a primeira nota: "))
+Nota_2 = float(input("Adicione a primeira nota: "))
+
+#Cálculando a média: 
+Cálculo_da_Média = (Nota_1 + Nota_2)/2
+
+#If, elif e else: 
+if Cálculo_da_Média < 5.0: 
