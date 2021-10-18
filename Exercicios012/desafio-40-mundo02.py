a1318515cd8b860762ec6890e4e5f1dca240508f
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-10-18 16:45:12 -0300

    Retirando uma quebra de linha
    Foi retirado um /n no final da frase do print na condicional elif, foi colocada apenas para teste.

--- Exercicios012/desafio-40-mundo02.py
+++ Exercicios012/desafio-40-mundo02.py
@@ -9,3 +9,4 @@ Cálculo_da_Média = (Nota_1 + Nota_2)/2
 
 #If, elif e else: 
 if Cálculo_da_Média < 5.0: 
+    print("De acordo com sua média {}, você foi REPROVADO sua média foi menor que 5.0! \nEstude mais na próxima!")
