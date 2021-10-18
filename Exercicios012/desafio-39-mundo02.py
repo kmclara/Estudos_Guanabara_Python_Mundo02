e38ea81ab81a00d4dd391ec4db231745de9bdead
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-10-18 15:26:03 -0300

    Mudança de nome das variaveis linhas 19 e 20

--- Exercicios012/desafio-39-mundo02.py
+++ Exercicios012/desafio-39-mundo02.py
@@ -8,3 +8,21 @@ Ano_de_Nascimento = int(input("Qual o ano do seu nascimento? "))
 
 #Fazendo a função do datetime:
 Ano_do_Computador = date.today().year
+
+#Cálculo para saber a idade do usuário: 
+Idade_do_Usuário = (Ano_do_Computador - Ano_de_Nascimento)
+
+#Delimitando a idade correta: 
+Idade_Correta = 18 
+
+#Cálculo para ver se falta ou se passou para realizar o alistamento:
+Anos_que_Falta = (Idade_Correta - Idade_do_Usuário)
+Anos_que_Passou = (Idade_do_Usuário - Idade_Correta)
+
+#Cálculo mostrando o ano que o usuário deve ou deveria se alistar: 
+Deve_se_Alistar = (Ano_do_Computador + Idade_Falta)
+Deveria_se_Alistar = (Ano_do_Computador - Idade_Passou)
+
+#If, elif e else
+if idade < 18: 
+    print("Você ainda não precisa se alistar! Falta {} anos para você se alistar. Você poderá comparecer no alistamento no ano de {}!".format(Idade_Falta, Deve_se_Alistar))
