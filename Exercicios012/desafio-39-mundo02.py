f495356f91e33bf8c2a7b9e9ac72c723412eaed8
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-10-18 15:36:59 -0300

    Add elif, else e print ao final do programa

--- Exercicios012/desafio-39-mundo02.py
+++ Exercicios012/desafio-39-mundo02.py
@@ -26,3 +26,9 @@ Deveria_se_Alistar = (Ano_do_Computador - Anos_que_Passou)
 #If, elif e else
 if Idade_do_Usuário < 18: 
     print("Você ainda não precisa se alistar! Falta {} anos para você se alistar. Você poderá comparecer no alistamento no ano de {}!".format(Anos_que_Falta, Deve_se_Alistar))
+elif Idade_do_Usuário == 18:
+    print("De acordo com seu ano de nascimento, você possui {} e já está na hora de fazer o alistamento militar!".format(Idade_do_Usuário))
+else: 
+    print("Você precisa se alistar! Já se passou {} anos da idade certa para o seu alistamento! Você deveria ter se alistado em: {}!".format(Anos_que_Passou, Deveria_se_Alistar))
+
+print("Obrigada por usar nosso programa! Volte sempre!")
