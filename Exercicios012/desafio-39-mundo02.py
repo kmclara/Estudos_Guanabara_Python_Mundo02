e8ef84f29cebd4263e5d20b28498132de1b11414
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-10-18 15:09:45 -0300

    Mudando os : para =
    Esqueci novamente que se usa = nas variaveis ao inves de :

--- Exercicios012/desafio-39-mundo02.py
+++ Exercicios012/desafio-39-mundo02.py
@@ -1,1 +1,10 @@
 #Desafio 39 - Alistamento Militar 
+
+#Importante o ano do pc: 
+from datetime import date 
+
+#Perguntando ao usuário seu ano de nascimento: 
+Ano_de_Nascimento = int(input("Qual o ano do seu nascimento? "))
+
+#Cálculo para saber a idade do usuário: 
+Idade_do_Usuário = 
