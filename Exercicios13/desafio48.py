7d251eb802a38377eacf727838d0a4f8099f9c16
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-11-26 11:55:10 -0300

    Somar apenas multiplos de tres com laço for
    Foi feito o programa com estes codigos assim
    Tentativa de de colocar tudo em uma linha com if

---
+++ Exercicios13/desafio48.py
@@ -0,0 +1,18 @@
+soma = 0
+for numero in range(0, 501):
+    multiplodetres = numero % 3
+    if multiplodetres == 0:
+        soma = soma + numero
+        print(numero)
+print(soma)
+
+
+soma = 0
+
+for numero in range(0, 501):
+    restoZero = numero % 3
+    eMultiploDeTres = (if restoZero == 0)
+    if eMultiploDeTres:
+        soma = soma + numero
+
+print(soma)
