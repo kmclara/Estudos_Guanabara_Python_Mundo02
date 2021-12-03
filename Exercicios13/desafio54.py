ef7a9b5ddc4c9ecb3cb1c7e77dc9d0c6344a56db
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-12-03 08:43:34 -0300

    Programa de contagem
    A proposta do programa era fazer um programa em que o usuario colocasse 7 datas de nascimento de pessoas diferentes e o programa mostrasse na tela quantas pessoas eram maiores de idades e quantas eram menores de idade.
    Inicialmente, importamos a biblioteca date pra conseguirmos pegar uma de suas funçoes que e a que le o ano do computador para que efetuemos o calculo junto ao ano colocado pelo usuario
    Foi usado depois dois contadores, contmaior e contmenor para fazermos a contagem dos mais velhos e mais novos, amabas as variaveis atribuimos a elas o valor 0
    Logo, fizemos a variavel AnoComputador colocando a funçao da biblioteca date para funcionar
    Em seguida, vem o laço de repetiçao for e logo dentro dele,o input sobre a data de nascimento
    Depois veio as condicionais if e elif e por fim o print com o resultado final

---
+++ Exercicios13/desafio54.py
@@ -0,0 +1,20 @@
+from datetime import date
+
+contmaior = 0
+contmenor = 0
+
+AnoComputador = date.today().year
+
+for Maioridade in range (0,7):
+    AnoNascimento = int(input("Digite seu ano de nascimento: "))
+    Conta = (AnoComputador-AnoNascimento)
+    if Conta <21:
+        contmenor+=1
+    elif Conta >=21:
+        contmaior+=1
+print("O total de pessoas maiores de idade é de {}.".format(contmaior))
+print("O total de pessoas menores de idade é de {}.".format(contmenor))
+
+
+      
+    
