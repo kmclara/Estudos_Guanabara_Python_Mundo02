2ddd5bdd2a6bbf04af43c031de37f12b8bfcca92
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-12-06 11:38:46 -0300

    Exercicio 56
    A proposta e perguntar a 4 pessoas a idade, o nome, e o sexo, e fazer uma media da idade dos 4, dizer o nome do homem mais velho e quantas mulheres tem menos de 20 anos
    Foi colocado o laço de repetiço for com a repetiçao de 1,5
    Adicionado inputs do nome, idade e sexo, e temporariamente a variavel para calculo da media das idades

---
+++ Exercicios13/desafio56.py
@@ -0,0 +1,7 @@
+
+for Questions in range (1,4):
+    NomeUsuario = input("Nome da {}ª pessoa: ".format(Questions))
+    IdadeUsuario = int(input("Idade da {}ª pessoa: ".format(Questions)))
+    SexoUsuario = input("Sexo da {}ª pessoa: ".format(Questions))
+    #Calculando a média de idade do grupo:
+    MediaIdade = 
