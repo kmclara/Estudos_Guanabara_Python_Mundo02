13f72abbd1af38ea1d80a30ccbd32679b9ea0bbc
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-12-06 11:08:44 -0300

    Adicionando o restante das condicionais
    Apos, adicionamos o Else. Dentro do else, colocamos um if dizendo que se o peso colocado for maior que a variavel maior que ate entao e do primeiro peso colocado, entao apartir dali, nao sera mais o primeiro peso maior, mas sim, o proximo, e a mesma logica segue para o menor.
    Logo depois, adicionamos os prints para que faça a analise de qual peso e maior e qual e menor dentro os 5

--- Exercicios13/desafio55.py
+++ Exercicios13/desafio55.py
@@ -5,6 +5,16 @@ menor = 0
 
 for Pesos in range (1,6):
     PerguntandoOPeso: float(input("Qual o peso da {}ª pessoa.".format(Pesos)))
+    #Se for o peso da primeira pessoa, ela será maior e menor ao mesmo tempo, pois não temos outro valor de comparação:
     if Pesos == 1:
         maior = Pesos
         menor = Pesos
+    else:
+        if PerguntandoOPeso > maior:
+            maior  = PerguntandoOPeso
+        if PerguntandoOPeso < menor:
+            menor = PerguntandoOPeso
+print("O maior peso lido foi de {}Kg!".format(maior))
+print("O menor peso lido foi de {}Kg!".format(menor))
+
+
