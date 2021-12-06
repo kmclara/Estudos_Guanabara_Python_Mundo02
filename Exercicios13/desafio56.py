91ba53f92454dabf1b9621d8039682e017840293
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-12-06 11:57:07 -0300

    Adicionando variaveis e condicionais
    Primeiramente, foi removido a variavel e media das idades e adicionado fora do laço de repetiçao uma variavel chamada SomaIdade e MediaIdade. A variavel SomaIdade que tem como valor , foi somada com as idades postas pelos usuarios, e fora do laço, a variavel MediaIdade possuiu o valor da divisao da SomaIdade/4. Ou seja, nos somamos as idades dos usuarios e dividimos por 4 que e a quantidade de usuarios que interagiram com o programa, assim que se calcula a media. Foi adicionado um print mostrando a media das idades. O calculo da media e o print com o resultado foi posto fora do laço de repetiçao.
    Logo depois, foi adicionado fora do laço de repetiçao mais duas variaveis: IdadeHomemVelho que tem como valor 0 e NomeHomemVelho que tem uma string vazia. Ambas as variveis sao para acharmos/analisarmos o nome do home mis velho do grupo das 4 pessoas que interagiram com nosso programa. Dentro do laço de repetiçao adicionamos uma condicional If dizendo que se o primeiro valor for adicionado pelo primeiro usuario e se o campo SexoUsuario for adicionado pelo usuario M ou m, entao a IdadeHomemVelho sera a IdadeUsuario e o NomeHomemVelho sera o NomeUsuario. Foi feito isso pelo mesmo motivo que o outro exercicio, nao temos e nao sabemos qual termo vamos comparar ainda, entao fazemos essa atribuiçao de antemao

--- Exercicios13/desafio56.py
+++ Exercicios13/desafio56.py
@@ -1,7 +1,15 @@
+SomaIdade = 0 
+MediaIdade = 0
+IdadeHomemVelho = 0
+NomeHomemVelho = ''
 
-for Questions in range (1,4):
+for Questions in range (1,5):
     NomeUsuario = input("Nome da {}ª pessoa: ".format(Questions))
     IdadeUsuario = int(input("Idade da {}ª pessoa: ".format(Questions)))
     SexoUsuario = input("Sexo da {}ª pessoa: ".format(Questions))
-    #Calculando a média de idade do grupo:
-    MediaIdade = 
+    SomaIdade += IdadeUsuario
+    if Questions == 1 and SexoUsuario in "Mm":
+        IdadeHomemVelho = IdadeUsuario
+        NomeHomemVelho = NomeUsuario
+MediaIdade = SomaIdade/4
+print("A média da idade do grupo é de {} anos!".format(MediaIdade))
